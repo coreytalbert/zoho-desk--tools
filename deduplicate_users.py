456cb9f0590d70ff073a8d0a23b74c1152bf2d4c
Corey Talbert <59098922+coreytalbert@users.noreply.github.com>
2024-05-06 16:37:10 -0400

    Update deduplicate_users.py
    Properly handling source id

--- deduplicate_users.py
+++ deduplicate_users.py
@@ -25,9 +25,12 @@ for contact in contact_map:
     if len(contact_map[contact]) > 1:
         duplicate_count+=1
 
+# pprint.pprint(contact_map)
 
-
+# v is a list of tuples of the form (createdTime, id)
 duplicated_contacts = {k:v for (k, v) in contact_map.items() if len(v) > 1}
+
+# pprint.pprint(duplicated_contacts)
 
 del duplicated_contacts[None]
 
@@ -35,12 +38,14 @@ with open("merge_contacts_api_payload.txt", 'w') as deduped_file:
 
     for (k, v) in duplicated_contacts.items():
     
-        v.sort(key=lambda tup: tup[0])
+        # Sorting by ascending createdTime here, and considering the oldest contact to be 
+        # the primary record into which all other records should be merged 
+        v.sort(key=lambda tup: tup[0]) 
 
         # https://desk.zoho.com/DeskAPIDocument#Contacts#Contacts_MergeContacts
         merge_dict = dict(
             {
-                "ids":[], 
+                "ids":[], # The list of contact ids to merge into the source id
                 "source":{
                     "firstName":"", 
                     "lastName":"", 
@@ -55,13 +60,16 @@ with open("merge_contacts_api_payload.txt", 'w') as deduped_file:
     
         for tup in v:
             merge_dict["ids"].append(tup[1])
+        del merge_dict["ids"][0] # This first value is the source id, it should not be in the list
 
         source_id = v[0][1]
 
         for attr in merge_dict.get("source").keys():
             merge_dict["source"][attr] = source_id
-    
+
+        deduped_file.write("merge_contacts_api_payloads.insert(")
         deduped_file.write(merge_dict.__str__())
+        deduped_file.write(");")
         deduped_file.write('\n')
         pprint.pprint(merge_dict)
 
